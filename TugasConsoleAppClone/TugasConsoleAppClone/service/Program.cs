f08d56a0f2572df7acc568d01ba8a1e552ce7593
Parlinggoman <89424392+Parlinggoman@users.noreply.github.com>
2021-08-24 18:52:34 +0700

    Create Program.cs

---
+++ TugasConsoleAppClone/TugasConsoleAppClone/service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TugasConsoleAppClone.view;
+
+namespace TugasConsoleAppClone.service
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            ViewMenu.MainMenu();
+        }
+    }
+}
